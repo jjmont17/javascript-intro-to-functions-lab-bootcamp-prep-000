22d9f6a7b91727c0bb86368114497e8b35ce06af
jmontenegro@uh.edu <jmontenegro@uh.edu>
2018-05-24 02:45:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function shout(string)
+{
+  'Hello!'.toUpperCase()
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  'HELLO!'.toLowerCase()
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  "Hello!".toUpperCase()
+  console.log(string.toUpperCase())
+}
+function logWhisper(string)
+{
+  "Hello!".toLowerCase()
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string)
+{
+
+if (string === string.toUpperCase())
+{
+  return string("I cant hear you!")
+}
+}
